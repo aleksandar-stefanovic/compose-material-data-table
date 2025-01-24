ada745cbf0a8cdbc5d0a6c6331365d2bb6c89270
Aleksandar Stefanović <stefanoviczaleksandar@gmail.com>
2025-01-24 18:48:10 +0100

    Quick fix for the WASM platform code

--- composeApp/src/wasmJsMain/kotlin/io/github/aleksandarstefanovic/composematerialdatatable/PlatformUtil.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/aleksandarstefanovic/composematerialdatatable/PlatformUtil.wasmJs.kt
@@ -6,10 +6,8 @@ actual fun LocalDate.format(format: String): String {
     return "${this.dayOfMonth}.${this.month}.${this.year}."
 }
 
-actual fun String.format(float: Float): String {
-    return float.toString()
-}
+actual fun String.format(float: Float): String = float.toString()
 
-actual fun String.format(double: Double): String {
-    return double.toString()
-}
+actual fun String.format(double: Double): String = double.toString()
+
+actual fun String.format(int: Int): String = int.toString()
--- composeApp/src/wasmJsMain/kotlin/io/github/aleksandarstefanovic/composematerialdatatable/Util.wasmJs.kt
+++
@@ -1,3 +0,0 @@
-package io.github.aleksandarstefanovic.composematerialdatatable
-
-actual fun String.format(int: Int): String =
