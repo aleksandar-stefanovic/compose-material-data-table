9e6ad08cd334a18c3fafed4c2b73b2f921d98949
Aleksandar Stefanović <stefanoviczaleksandar@gmail.com>
2025-03-17 23:12:04 +0100

    Make pagination bar responsive

--- lib/src/commonMain/kotlin/io/github/aleksandar_stefanovic/composematerialdatatable/PaginationBar.kt
+++ lib/src/commonMain/kotlin/io/github/aleksandar_stefanovic/composematerialdatatable/PaginationBar.kt
@@ -2,7 +2,9 @@ package io.github.aleksandar_stefanovic.composematerialdatatable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -52,40 +54,45 @@ internal fun PaginationBar(
         )
     }
 
-    Row(modifier, Arrangement.End, Alignment.CenterVertically) {
-        Text("Rows per page", Modifier.padding(end = 4.dp))
-        DropdownPicker(pageSize, pageSizeOptions, onOptionPicked = { pageSize = it })
-        val firstIndex = pageIndex * pageSize + 1
-        val lastIndex = ((pageIndex + 1) * pageSize).coerceAtMost(dataSize)
-        Text("$firstIndex-$lastIndex of $dataSize", Modifier.padding(horizontal = 16.dp))
-        IconButton(
-            onClick = {
-                pageIndex = 0
-            },
-            Modifier.padding(end = 4.dp).alpha(if (pageIndex > 0) 1f else 0.38f),
-            enabled = pageIndex > 0
-        ) { Image(vectorResource(Res.drawable.first_page), "Jump to start") }
-        IconButton(
-            onClick = {
-                pageIndex--
-            },
-            Modifier.padding(end = 4.dp).alpha(if (pageIndex > 0) 1f else 0.38f),
-            enabled = pageIndex > 0
-        ) { Image(vectorResource(Res.drawable.chevron_left), "Previous page") }
-        IconButton(
-            onClick = {
-                pageIndex++
-            },
-            Modifier.padding(end = 4.dp).alpha(if (pageIndex < pageCount - 1) 1f else 0.38f),
-            enabled = pageIndex < pageCount - 1
-        ) { Image(vectorResource(Res.drawable.chevron_right), "Next page") }
-        IconButton(
-            onClick = {
-                pageIndex = pageCount - 1
-            },
-            Modifier.padding(end = 4.dp).alpha(if (pageIndex < pageCount - 1) 1f else 0.38f),
-            enabled = pageIndex < pageCount - 1
-        ) { Image(vectorResource(Res.drawable.last_page), "Jump to end") }
+    BoxWithConstraints(modifier) {
+        val showPageSize = maxWidth >= 500.dp
+        Row(Modifier.fillMaxWidth(), Arrangement.End, Alignment.CenterVertically) {
+            if (showPageSize) {
+                Text("Rows per page", Modifier.padding(end = 4.dp))
+                DropdownPicker(pageSize, pageSizeOptions, onOptionPicked = { pageSize = it })
+            }
+            val firstIndex = pageIndex * pageSize + 1
+            val lastIndex = ((pageIndex + 1) * pageSize).coerceAtMost(dataSize)
+            Text("$firstIndex-$lastIndex of $dataSize", Modifier.padding(horizontal = 16.dp))
+            IconButton(
+                onClick = {
+                    pageIndex = 0
+                },
+                Modifier.padding(end = 4.dp).alpha(if (pageIndex > 0) 1f else 0.38f),
+                enabled = pageIndex > 0
+            ) { Image(vectorResource(Res.drawable.first_page), "Jump to start") }
+            IconButton(
+                onClick = {
+                    pageIndex--
+                },
+                Modifier.padding(end = 4.dp).alpha(if (pageIndex > 0) 1f else 0.38f),
+                enabled = pageIndex > 0
+            ) { Image(vectorResource(Res.drawable.chevron_left), "Previous page") }
+            IconButton(
+                onClick = {
+                    pageIndex++
+                },
+                Modifier.padding(end = 4.dp).alpha(if (pageIndex < pageCount - 1) 1f else 0.38f),
+                enabled = pageIndex < pageCount - 1
+            ) { Image(vectorResource(Res.drawable.chevron_right), "Next page") }
+            IconButton(
+                onClick = {
+                    pageIndex = pageCount - 1
+                },
+                Modifier.padding(end = 4.dp).alpha(if (pageIndex < pageCount - 1) 1f else 0.38f),
+                enabled = pageIndex < pageCount - 1
+            ) { Image(vectorResource(Res.drawable.last_page), "Jump to end") }
+        }
     }
 }
 
