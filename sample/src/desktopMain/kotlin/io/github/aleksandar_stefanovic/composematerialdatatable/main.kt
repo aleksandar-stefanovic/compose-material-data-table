db0dd66e3d7d4325a9b0c34113f614341f47777f
Aleksandar Stefanović <stefanoviczaleksandar@gmail.com>
2025-07-28 12:49:48 +0200

    Initial implementation of cell editing for Text columns

--- sample/src/desktopMain/kotlin/io/github/aleksandar_stefanovic/composematerialdatatable/main.kt
+++ sample/src/desktopMain/kotlin/io/github/aleksandar_stefanovic/composematerialdatatable/main.kt
@@ -1,13 +1,19 @@
 package io.github.aleksandar_stefanovic.composematerialdatatable
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose Material Data Table",
+        state = rememberWindowState(WindowPlacement.Floating, false, WindowPosition.PlatformDefault, 1000.dp)
     ) {
+
         App()
     }
 }
